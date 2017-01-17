b71bd0fa5d23cfbbab94f9a6375d28f33d22af09
Cole Erickson <pawnstar4@gmail.com>
2017-01-16 21:46:01 -0700

    Script for parsing csv

---
+++ statConfig.js
@@ -0,0 +1,92 @@
+var stats = {
+  'Points': {
+    'stat':'Score',
+    'trim': 0,
+    'graph': false,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'K/D': {
+    'stat':'K/D',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'DG/DR': {
+    'stat':'DG/DR',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'DG/K': {
+    'stat':'DG/K',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return first.current - second.current;
+    }
+  },
+  'DR/D': {
+    'stat':'DR/D',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'Lifespan': {
+    'stat':'Lfspn',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'Killspan': {
+  'trim':10,
+    'stat':'Klspn',
+    'graph': true,
+    'sort':function(first,second){
+      return first.current - second.current;
+    }
+  },
+  'DPS': {
+    'stat':'DPS',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  },
+  'DRPS': {
+    'stat':'DRPS',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return first.current - second.current;
+    }
+  },
+  'Peak Damage': {
+    'trim':10,
+    'stat':'Peak Damage',
+    'graph': true,
+    'sort':function(first,second){
+      return second.current.max - first.current.max;
+    }
+  },
+  'Hit %': {
+    'stat':'Hit%',
+    'trim':10,
+    'graph': true,
+    'sort':function(first,second){
+      return second.current - first.current;
+    }
+  }
+}
+
+module.exports = stats;
---
+++ usefulFunctions.js
@@ -0,0 +1,91 @@
+var stuff = {};
+
+stuff.iterateMatches = function(weeks, callback){
+  //Callback gets (thisMatch, prevMatch, weekNum)
+  var prevMatch = null;
+  for(var i = 0; i < weeks.length; i++){
+    for(var j = 0; j < weeks[i].length; j++){
+      callback(weeks[i][j], prevMatch, i);
+      prevMatch = weeks[i][j];
+    }
+  }
+};
+
+stuff.doTime = function(weeks){
+  stuff.iterateMatches(weeks, function(current){
+    if(current.hasOwnProperty('Min') && current.hasOwnProperty('Sec'))
+      current.Time = current['Min']*60 + current['Sec'];
+  })
+};
+
+stuff.totalSomething = function(weeks, statName, totalName){
+  stuff.iterateMatches(weeks,function(current,prev){
+    var total = null;
+    if(prev && prev.hasOwnProperty(totalName))
+      total = prev[totalName];
+
+    if(current.hasOwnProperty(statName)){
+      if(total)
+        total += current[statName];
+      else
+        total = current[statName];
+      current[totalName] = total;
+    }else{
+      current[totalName] = total;
+    }
+  })
+};
+
+
+stuff.maxSomething = function(weeks, statName, maxName){
+  stuff.iterateMatches(weeks, function(current, prev, week){
+    if(!current.hasOwnProperty(statName)){
+      if(prev && prev.hasOwnProperty(maxName))
+        current[maxName] = prev[maxName];
+      else
+        current[maxName] = null;
+    }else{
+      if(prev && prev.hasOwnProperty(maxName) && prev[maxName] && prev[maxName].max >= current[statName])
+        current[maxName] = prev[maxName];
+      else
+        current[maxName] = {
+          'max': current[statName],
+          'week': week + 1 // Plus one because it's not supposed to display zero-based index
+        }
+    }
+  })
+};
+
+stuff.divideSomething = function(weeks, numerator, denominator, resultName){
+  stuff.iterateMatches(weeks, function(current){
+    if(current.hasOwnProperty(numerator) && current.hasOwnProperty(denominator))
+      current[resultName] = current[numerator] / current[denominator];
+  })
+};
+
+stuff.multiplySomething = function(weeks, first, second, resultName){
+  stuff.iterateMatches(weeks, function(current){
+    if(current.hasOwnProperty(first) && current.hasOwnProperty(second))
+      current[resultName] = current[first] * current[second];
+  })
+};
+
+stuff.trimSomething = function(weeks, stat){
+  stuff.iterateMatches(weeks, function(current){
+    if(current.hasOwnProperty(stat))
+      delete current[stat];
+  })
+}
+stuff.countMatches = function(weeks){
+  var count = 0;
+  stuff.iterateMatches(weeks, function(current){
+    if(current.hasOwnProperty('Rnk') && current['Rnk'] != 'Lt' && current['Rnk'] != 'No'){
+      count++;
+    }
+    current.matchCount = count;
+  })
+
+  return count;
+}
+
+module.exports = stuff;
